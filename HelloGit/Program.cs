6ce269354acfeb75eccfd584ffc061eb45c4a39f
Steve Pham <spham@corelogic.com>
2016-07-01 05:12:38 -0700

    added Foo

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -8,10 +8,35 @@ namespace HelloGit
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
-			Console.WriteLine("Hello World, from Git!!!!!");
-			Console.ReadLine();
+			try
+			{
+				var foo = new Foo();
+				foo.Bar = "None";
+				Console.WriteLine("Hello {0}, from Git!!!!!", foo.Bar);
+
+				foo.Doit();
+
+				Console.ReadLine();
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return 1;
+			}
+		}
+	}
+
+
+	class Foo
+	{
+		public string Bar { get; set; }
+
+		public void Doit()
+		{
+			throw new ArgumentException("Blah Blah");
 		}
 	}
 }
