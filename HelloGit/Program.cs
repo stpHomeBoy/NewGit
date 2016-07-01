1d15ecfa1961152645c38fb734d4b3e46e49e595
stpHomeBoy <stpHomeBoy@users.noreply.github.com>
2016-07-01 05:00:28 -0700

    Update Program.cs
    update hello world

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -10,7 +10,7 @@ namespace HelloGit
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, Git!");
+			Console.WriteLine("Hello world, from Git!!!!!");
 			Console.ReadLine();
 		}
 	}
